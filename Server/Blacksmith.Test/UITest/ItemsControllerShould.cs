ffdfb774098646d5d14e9c07e40816964f7e1cc5
Karol Topchiev <karoltopchiev@outlook.com>
2025-04-14 18:52:02 +0300

    More tests

--- Server/Blacksmith.Test/UITest/ItemsControllerShould.cs
+++ Server/Blacksmith.Test/UITest/ItemsControllerShould.cs
@@ -24,7 +24,7 @@ public class ItemsControllerShould
         _itemsController = new ItemsController(_itemAdderService.Object, _itemGetterService.Object);
     }
 
-    #region GetterService
+    #region GetterServiceFail
     [Fact]
     public async Task GetAllItemsAsync_ShouldReturnNoContent_WhenArgumentIsNull()
     {
@@ -72,6 +72,57 @@ public class ItemsControllerShould
         var noContentResult = Assert.IsType<NoContentResult>(result.Result);
 
         Assert.Equal(204, noContentResult.StatusCode);
+    }
+
+    #endregion
+
+    #region GetterServiceOK
+
+    [Fact]
+    public async Task GetAllITemsAsync_ShouldReturnAllItems()
+    {
+        //Arrange
+        ItemParams itemParams = new ItemParams() { CurrentPageNumber = 1, ItemsOnPage = 12 };
+        Guid itemId = new Guid("957602a8-e3b1-4132-b8f2-794ca22163f2");
+        ItemResponse item = new ItemResponse() { ItemId = itemId, Name = "Armor", Price = 2000 };
+        var items = new List<ItemResponse>() { item };
+
+        _itemGetterService.Setup(service => service.GetAllItemsAsync(itemParams))
+                            .ReturnsAsync(new PaginatedList<ItemResponse>(items!, 1, 1, 12));
+
+        //Act
+        var result = await _itemsController.GetAllItemsAsync(itemParams);
+
+        //Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var actualItems = Assert.IsType<PaginatedList<ItemResponse>>(okResult.Value);
+
+        Assert.Equal(items, actualItems.Items!);
+        Assert.Single(actualItems.Items);
+        Assert.Equal(itemParams.CurrentPageNumber, actualItems.CurrentPageNumber);
+        Assert.Equal(itemParams.ItemsOnPage, actualItems.ItemsOnPage);
+    }
+
+    [Fact]
+    public async Task GetItemByIdAsync_ShouldReturnExpectedResponse()
+    {
+        //Arrange
+        Guid itemId = Guid.NewGuid();
+        string name = "Armor";
+        double price = 2000;
+        ItemResponse expectedResponse = new ItemResponse() { ItemId = itemId, Name = name, Price = price };
+
+        _itemGetterService.Setup(s => s.GetItemByIdAsync(itemId)).ReturnsAsync(expectedResponse);
+
+        //Act
+        var result = await _itemsController.GetItemByIdAsync(itemId);
+
+        //Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var actualItem = Assert.IsType<ItemResponse>(okResult.Value);
+
+        Assert.Equal(itemId, actualItem.ItemId);
+        Assert.Equal(expectedResponse, actualItem);
     }
 
     #endregion
